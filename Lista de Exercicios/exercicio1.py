fcb344d87c361af34186c115869315f802f46700
Igor Eduardo <50914198+M0oses@users.noreply.github.com>
2020-08-08 19:13:40 -0300

    Screenshoots dos códigos e resultados no readme
    Screenshoots dos códigos e resultados no readme

---
+++ Lista de Exercicios/exercicio1.py
@@ -0,0 +1,11 @@
+#1-Faça um programa que receba a idade 
+#do usuário e diga se ele é maior ou menor de idade.   
+
+#idade = 18
+
+idade = int(input("Digite sua idade:"))
+
+if idade >= 18:
+	print("Você é maior de idade")
+else:
+	print("Você é menor de idade")
