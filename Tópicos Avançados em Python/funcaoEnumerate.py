e18132e499e5b8475d2003c51147277cdf6bfa8e
Igor Eduardo <50914198+M0oses@users.noreply.github.com>
2020-08-08 19:17:20 -0300

    Screenshots dos códigos e resultados no readme
    Screenshots dos códigos e resultados no readme

---
+++ Tópicos Avançados em Python/funcaoEnumerate.py
@@ -0,0 +1,11 @@
+#Função Enumerate
+
+lista = ["chuchu", "batata", "repolho", "cenoura"]
+
+for i, nome in enumerate(lista):
+	print(i, nome)
+
+"""
+for i in range(len(lista)):
+	print(i,lista[i])
+	"""
