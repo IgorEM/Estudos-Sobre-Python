afc8b61c33a333db576c9759ffc3d7af7033b11c
Igor Eduardo <50914198+M0oses@users.noreply.github.com>
2020-08-08 18:18:30 -0300

    olamundo.py

---
+++ olamundo.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+print("Olá mundo!")
+#pode usar o GoogleColad para rodar os códigos
+"""
+Isto 
+é
+um
+comentário
+"""
+print (2*2)
+print (2/2)
+print (2+2)
+print (2-2)
+print (2**3) #exponenciação r=8
+print (10%3) #modulo, da o resto
+
+minha_variavel = "Essa é minha váriavel"
+print(minha_variavel)
