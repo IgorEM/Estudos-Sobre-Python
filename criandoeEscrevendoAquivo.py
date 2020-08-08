2329a6a9f1ede6e0cd172a0e67b65ad5986b31d0
Igor Eduardo <50914198+M0oses@users.noreply.github.com>
2020-08-08 19:26:54 -0300

    Screenshots dos códigos e resultados no readme
    Screenshots dos códigos e resultados no readme

--- criandoeEscrevendoAquivo.py
+++ criandoeEscrevendoAquivo.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-criando_arquivo = open("arquivo3.txt", "a") #nao esquecer do open e do w
+criando_arquivo = open("arquivo3.txt", "w") #nao esquecer do open e do w
 
 criando_arquivo.write("Escrevendo num arquivo já existente\n")
 
